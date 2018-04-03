a1c57f01473051d58b7cdddab417f6e217839830
Kumar Prateek Viraj <krprateekviraj+github@gmail.com>
2018-04-03 11:15:48 +0530

    Updated
    Commented last line.
    That was redundant. However, haven't tried running the latest version.

--- q11 conc.pl
+++ q11 conc.pl
@@ -4,4 +4,8 @@
    conc([], L, L).
    conc(L, [], L).  
    conc([H|T], L2, [H|L3]) :- conc(T, L2, L3).
-   conc(X, Y, L3) :- conc(X, [Y], L3).
+  
+  %Try uncommenting this, if it doesnt work.
+  %However this is redundant.
+  
+  % conc(X, Y, L3) :- conc(X, [Y], L3).
